ebc160728e7f1d04350781e29922cd513aee0aa7
ivomasi <ivo.masiar7@gmail.com>
2022-07-27 13:36:32 +0200

    added input to searchbar

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -2,43 +2,45 @@ import React from "react";
 
 import styled from "styled-components";
 
+import { StyledInput } from "../../shared/Input/Input";
+import { FlexCenter } from "../../shared/styles/Common";
+
 function SearchBar() {
+	const handleSubmit = (e) => {
+		e.preventDefault();
+
+		console.log(e);
+	};
+
 	return (
-		<Search>
+		<Search onSubmit={handleSubmit}>
 			<SearchText type="text" placeholder="search"></SearchText>
 			<SearchCat></SearchCat>
+			<SubmitSearch type="submit">Search</SubmitSearch>
 		</Search>
 	);
 }
+const Search = styled.form`
+	${FlexCenter}
+	flex-basis: 66%;
+`;
 
-const SearchText = styled.input`
-    flex-basis: 60%;
-    height: 2rem;
-    line-height: 2rem;
-    padding: 1rem 2rem;
-
-    :focus {
-        outline: none;
-    }
-
-    :placeholder-shown {
-        text-transform: capitalize;
-    }
+const SearchText = styled(StyledInput)`
+	flex-basis: 60%;
 `;
 
 const SearchCat = styled.select`
-    flex-basis: 25%;
-    height: 2rem;
-    line-height: 2rem;
-    padding: 1rem 2rem;
-
+	flex-basis: 25%;
+	height: 2rem;
+	line-height: 2rem;
+	padding: 1rem 2rem;
 `;
 
-const Search = styled.div`
-    flex-basis: 66%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-`
+const SubmitSearch = styled.button`
+	${FlexCenter}
+	height: 2rem;
+	line-height: 2rem;
+	padding: 1rem 2rem;
+`;
 
 export default SearchBar;
