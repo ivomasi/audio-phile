721698c63062c4b176737510ef8db44e1e2c5f2a
ivomasi <ivo.masiar7@gmail.com>
2022-07-29 17:55:05 +0200

    created pages for mixpults and speakrs, added to routing

--- src/routing/routing.js
+++ src/routing/routing.js
@@ -1,5 +1,7 @@
 import Home from "../pages/Home";
 
 import Headphones from "../pages/Headphones";
+import MixPults from "../pages/MixPults";
+import Speakers from "../pages/Speakers";
 
-export const routing = [{ name: "home", path: "/", component: Home, exact: true }, { name: "headphones", path: "/headphones", component: Headphones}];
+export const routing = [{ name: "home", path: "/", component: Home, exact: true }, { name: "headphones", path: "/headphones", component: Headphones},{ name: "speakers", path: "/speakers", component: Speakers},{ name: "mix pults", path: "/mix-pults", component: MixPults}];
