bad5b27f4a211d3bf5a87a8f3fe1d971131344cb
ivomasi <ivo.masiar7@gmail.com>
2022-07-29 13:53:59 +0200

    changing size through props in button

--- src/components/Button.js/Button.js
+++ src/components/Button.js/Button.js
@@ -1,22 +1,28 @@
 import React from "react";
 
-import styled from "styled-components";
+import styled, { createGlobalStyle } from "styled-components";
 
 import {flexCenter} from "../../shared/styles/common.js"
 
 
-function Button({ type = "button", text, onClick }) {
+function Button({ type = "button", text, onClick, size="m" }) {
 	return (
-		<Btn type={type} onClick={onClick}>
+		<Btn type={type} onClick={onClick} size={size} >
 			{text}
 		</Btn>
 	);
 }
 
+const BUTTON_SIZE = {
+    lg: "3rem",
+    m: "2rem",
+    sm: "1rem",
+}
+
 const Btn = styled.button`
 	${flexCenter}
-	height: 2rem;
-	line-height: 2rem;
+	height: ${(props) => BUTTON_SIZE[props.size]};
+	line-height: ${(props) => BUTTON_SIZE[props.size]};
 	padding: 1rem 2rem;
 `;
 
