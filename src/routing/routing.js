fb470bfd23423777fbd7343e54d93ebac236e501
ivomasi <ivo.masiar7@gmail.com>
2022-08-01 10:59:58 +0200

    add product page to routing

--- src/routing/routing.js
+++ src/routing/routing.js
@@ -1,7 +1,16 @@
 import Home from "../pages/Home";
+import { Route } from "react-router-dom";
 
 import Headphones from "../pages/Headphones";
 import MixPults from "../pages/MixPults";
 import Speakers from "../pages/Speakers";
+import Product from "../pages/Product";
 
-export const routing = [{ name: "home", path: "/", component: Home, exact: true }, { name: "headphones", path: "/headphones", component: Headphones},{ name: "speakers", path: "/speakers", component: Speakers},{ name: "mix pults", path: "/mix-pults", component: MixPults}];
+export const routing = [
+	{ name: "home", path: "/", component: Home, exact: true },
+	{ name: "headphones", path: "/headphones", component: Headphones },
+	{ name: "speakers", path: "/speakers", component: Speakers },
+	{ name: "mix pults", path: "/mix-pults", component: MixPults },
+	{ name: "product", path: "/audio/:productId", component: Product },
+
+];
