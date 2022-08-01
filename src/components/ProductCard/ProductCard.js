8e86d3e86e3f464abef49d41ca72f4b7bb189bde
ivomasi <ivo.masiar7@gmail.com>
2022-08-01 21:23:01 +0200

    added new fonts, make product card sticky

--- src/components/ProductCard/ProductCard.js
+++ src/components/ProductCard/ProductCard.js
@@ -3,7 +3,7 @@ import React from "react";
 //css
 import styled from "styled-components";
 import colors from "../../styled-system/colors";
-import {between, down} from "styled-breakpoints"
+import { between, down } from "styled-breakpoints";
 
 //comps
 import Button from "../Button/Button";
@@ -14,26 +14,33 @@ import svgImages from "../../assets";
 
 const { productCard } = svgImages;
 
-
 function ProductCard({ model, brand, price, stock, sold }) {
-
-
 	return (
 		<Card>
 			<h2>{`${brand} ${model}`}</h2>
 			{sold > 240 && <span>best seller</span>}
 			<p className="price">{price} €</p>
-			<StockText color={stock >=  10 ? colors.green :( stock === 0 ) ? colors.grey : colors.secondaryColor }>
-				{stock >= 10 ? "In stock" : (stock === 0 ) ? "OUT OF STOCK" : "running out"}
+			<StockText color={stock >= 10 ? colors.green : stock === 0 ? colors.grey : colors.secondaryColor}>
+				{stock >= 10 ? "In stock" : stock === 0 ? "OUT OF STOCK" : "running out"}
 			</StockText>
 			<p>Ships Same Business Day (Monday - Friday)</p>
-			{<Button primary style={{backgroundColor: stock !== 0 ? null : colors.grey}} rounded text={stock !== 0 ? "add to cart" : "OUT OF STOCK"} size="lg" width="100%" disabled={stock !== 0 ? false : true }/>}
+			{
+				<Button
+					primary
+					style={{ backgroundColor: stock !== 0 ? null : colors.grey }}
+					rounded
+					text={stock !== 0 ? "add to cart" : "OUT OF STOCK"}
+					size="lg"
+					width="100%"
+					disabled={stock !== 0 ? false : true}
+				/>
+			}
 
-            <ShipIcons>
-            {productCard.map(({ img,  title }, index) => {
-						return <InfoCard key={index} IconComponent={<Svg url={img} alt={title}  />}  title={title} />;
-					})}
-            </ShipIcons>
+			<ShipIcons>
+				{productCard.map(({ img, title }, index) => {
+					return <InfoCard key={index} IconComponent={<Svg url={img} alt={title} />} title={title} />;
+				})}
+			</ShipIcons>
 		</Card>
 	);
 }
@@ -48,6 +55,9 @@ const Card = styled.div`
 	align-items: flex-start;
 	max-height: 80vh;
 	flex-basis: 33%;
+	position: sticky;
+	align-self: stretch;
+	top: 25px;
 
 	${down("md")} {
 		max-height: inherit;
@@ -72,21 +82,18 @@ const StockText = styled.b`
 	color: ${(props) => props.color};
 `;
 
-
 const ShipIcons = styled.div`
 	display: grid;
 	grid-template-columns: 1fr 1fr 1fr 1fr;
-    grid-template-rows: 1fr;
+	grid-template-rows: 1fr;
 	grid-gap: 0.5rem;
 
 	${between("md", "xl")} {
 		grid-template-columns: 1fr 1fr;
-
 	}
 	${down("md")} {
 		grid-template-columns: 1fr;
-	align-self: center;
-
+		align-self: center;
 	}
 `;
 
